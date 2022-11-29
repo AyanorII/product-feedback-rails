d381bc2e8de4506cf3333e4fc75a9e8535e892a5
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-11-29 15:41:31 +0900

    add users to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 require 'json'
 require 'faker'
+
+# rubocop:disable Metrics/MethodLength
 class Seeder
   def initialize
     serialized_data = File.read('db/data.json')
@@ -30,17 +32,20 @@ class Seeder
     puts ''
     puts 'Creating users...'
 
-    10.times do
-      user =
-        User.create!(
-          email: Faker::Internet.email,
-          password: Faker::Internet.password,
-          username: Faker::Internet.username,
-          name: Faker::Name.name,
-          photo: Faker::Avatar.image,
-        )
+    users = JSON.parse(File.read('db/users.json'))
 
-      puts "Created user: #{user.username}"
+    users.each do |user|
+      first_name = user['name'].split(' ').first.downcase
+
+      User.create!(
+        email: Faker::Internet.email,
+        password: Faker::Internet.password,
+        username: user['username'],
+        name: user['name'],
+        photo: "#{ActionController::Base.helpers.image_path(first_name)}.jpg",
+      )
+
+      puts "Created user: #{user['username']}"
     end
 
     puts "Finished seeding users. #{User.count} users created!"
@@ -57,16 +62,21 @@ class Seeder
     )
   end
 
-  def create_comment(content, product, parent = nil)
-    if parent
+  def create_comment(attr = {})
+    if attr[:parent]
       Comment.create!(
-        content: content,
-        product: product,
-        user: User.all.sample,
-        parent: parent,
+        content: attr[:content],
+        product: attr[:product],
+        user: User.find_by(username: attr[:user]),
+        parent: attr[:parent],
+        replying_to: User.find_by(username: attr[:replying_to]),
       )
     else
-      Comment.create!(content: content, product: product, user: User.all.sample)
+      Comment.create!(
+        content: attr[:content],
+        product: attr[:product],
+        user: User.find_by(username: attr[:user]),
+      )
     end
   end
 
@@ -74,13 +84,28 @@ class Seeder
     puts "\nCreating comments for product: #{product.title}..."
 
     comments.each do |product_comment|
-      created_comment = create_comment(product_comment['content'], product)
+      created_comment =
+        create_comment(
+          {
+            content: product_comment['content'],
+            user: product_comment['user']['username'],
+            product: product,
+          },
+        )
 
       puts 'Created comment'
 
       replies = product_comment['replies']
       replies&.each do |reply|
-        create_comment(reply['content'], product, created_comment)
+        create_comment(
+          {
+            content: reply['content'],
+            user: reply['user']['username'],
+            product: product,
+            parent: created_comment,
+            replying_to: reply['replyingTo'],
+          },
+        )
 
         puts 'Created reply'
       end
